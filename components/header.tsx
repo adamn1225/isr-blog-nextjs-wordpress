c410c6f5cfee93a3bc2db011a584f29790c9aaf4
Adam Noah <42944389+adamn1225@users.noreply.github.com>
2024-05-19 01:04:43 +0000

    logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
-        Blog
+        <Image
+          src="/workspaces/isr-blog-nextjs-wordpress/public/favicon/paragon-van-lines-nob-logo-509w.png"
+          alt="Paragon Van Lines Logo"
+        />
       </Link>
       .
     </h2>
